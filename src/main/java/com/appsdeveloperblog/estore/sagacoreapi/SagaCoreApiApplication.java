c4f2a3cd5af6f6cf9e8ab030509fcaf06767506d
NashidForbes <NashidForbes@biohumans.com>
2021-12-28 16:55:35 -0500

    Initial commit.

---
+++ src/main/java/com/appsdeveloperblog/estore/sagacoreapi/SagaCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.estore.sagacoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaCoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appsdeveloperblog/estore/sagacoreapi/SagaCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.estore.sagacoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
