f6aea87592158c9bde718bef6f18cd22170243d9
NashidForbes <NashidForbes@biohumans.com>
2022-01-01 01:57:39 -0500

    Fetch User Payment details.

--- src/main/java/com/appsdeveloperblog/estore/sagacoreapi/query/FetchUserPaymentDetailsQuery.java
+++ src/main/java/com/appsdeveloperblog/estore/sagacoreapi/query/FetchUserPaymentDetailsQuery.java
@@ -1,8 +1,12 @@
 package com.appsdeveloperblog.estore.sagacoreapi.query;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FetchUserPaymentDetailsQuery {
     private String userId;
 }
