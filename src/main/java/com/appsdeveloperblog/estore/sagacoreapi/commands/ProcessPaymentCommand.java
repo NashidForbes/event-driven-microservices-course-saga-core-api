7f8a8f5cd2a808737c68d56c5b288de138901285
NashidForbes <NashidForbes@biohumans.com>
2022-01-03 15:56:59 -0500

    Approve the OrderApprovedEvent in OrderSaga and end the Saga lifecycle. Now fully working, check commits to see changes to fix issue with "aggregate not found in the event store" in class PaymentAggregate.

--- src/main/java/com/appsdeveloperblog/estore/sagacoreapi/commands/ProcessPaymentCommand.java
+++ src/main/java/com/appsdeveloperblog/estore/sagacoreapi/commands/ProcessPaymentCommand.java
@@ -1,12 +1,15 @@
 package com.appsdeveloperblog.estore.sagacoreapi.commands;
 
 import com.appsdeveloperblog.estore.sagacoreapi.models.PaymentDetails;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Builder
 @Data
+@AllArgsConstructor
 public class ProcessPaymentCommand {
 
     @TargetAggregateIdentifier
--- src/main/java/com/appsdeveloperblog/estore/sagacoreapi/events/PaymentProcessedEvent.java
+++ src/main/java/com/appsdeveloperblog/estore/sagacoreapi/events/PaymentProcessedEvent.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.estore.sagacoreapi.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
