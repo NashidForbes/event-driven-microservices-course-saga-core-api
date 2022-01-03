550a6c81762e61c5ea97aa48bbd7ff2139487225
NashidForbes <NashidForbes@biohumans.com>
2022-01-02 23:20:23 -0500

    Adding PaymentProcessedEvent to SagaCoreApi core library.

---
+++ src/main/java/com/appsdeveloperblog/estore/sagacoreapi/events/PaymentProcessedEvent.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.estore.sagacoreapi.events;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PaymentProcessedEvent {
+    private final String paymentId;
+    private final String orderId;
+}
